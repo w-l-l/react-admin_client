b193c484496e1442123ca733f5552b5d33454255
w-l-l <1291761009@qq.com>
2021-07-07 13:37:46 +0800

    富文本上传本地图片显示

--- src/components/RichTextEditor/index.jsx
+++ src/components/RichTextEditor/index.jsx
@@ -15,14 +15,36 @@ export default class RichTextEditor extends Component {
   }
   // 获取详情内容
   getDetail = _ => draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
+  // 图片上传回调
+  uploadCallback = file => new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('POST', '/api/manage/img/upload')
+    const data = new FormData()
+    data.append('image', file)
+    xhr.send(data)
+    xhr.addEventListener('load', _ => {
+      const res = JSON.parse(xhr.responseText)
+      const { data: { url } } = res
+      resolve({ data: { link: url } })
+    })
+    xhr.addEventListener('error', _ => {
+      const error = JSON.parse(xhr.responseText)
+      reject(error)
+    })
+  })
   render () {
     const { editorState } = this.state
+    const { onEditorStateChange, uploadCallback } = this
     return (
       <div>
         <Editor
           editorState={editorState}
           editorStyle={{border: '1px solid', minHeight: 200, paddingLeft: 10}}
-          onEditorStateChange={this.onEditorStateChange}
+          onEditorStateChange={onEditorStateChange}
+          toolbar={{
+            image: { uploadCallback },
+            alt: { present: true, mandatory: true }
+          }}
         />
       </div>
     )
