056137a15309b66cd276e9c580489d02d484882e
w-l-l <wll_rookie@aliyun.com>
2021-11-14 16:14:07 +0800

    饼图

--- src/pages/Charts/Pie/index.jsx
+++ src/pages/Charts/Pie/index.jsx
@@ -1,7 +1,134 @@
 import React, { Component } from 'react'
+import { Card } from 'antd'
+import ReactEcharts from 'echarts-for-react'
 
 export default class Pie extends Component {
-  render () {
-    return <h1>Pie</h1>
+  state = {
+    load: false
+  }
+  getOption = _ => {
+    return {
+      title: {
+        text: '某站点用户访问来源',
+        subtext: '纯属虚构',
+        x: 'center'
+      },
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+      },
+      series: [
+        {
+          name: '访问来源',
+          type: 'pie',
+          radius: '55%',
+          center: ['50%', '60%'],
+          data: [
+            { value: 335, name: '直接访问' },
+            { value: 310, name: '邮件营销' },
+            { value: 234, name: '联盟广告' },
+            { value: 135, name: '视频广告' },
+            { value: 1548, name: '搜索引擎' }
+          ],
+          itemStyle: {
+            emphasis: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    }
+  }
+  getOption2 = _ => {
+    return {
+      backgroundColor: '#2c343c',
+      title: {
+        text: 'Customized Pie',
+        left: 'center',
+        top: 20,
+        textStyle: {
+          color: '#ccc'
+        }
+      },
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)'
+      },
+      visualMap: {
+        show: false,
+        min: 80,
+        max: 600,
+        inRange: {
+          colorLightness: [0, 1]
+        }
+      },
+      series: [
+        {
+          name: '访问来源',
+          type: 'pie',
+          radius: '55%',
+          center: ['50%', '50%'],
+          data: [
+            { value: 335, name: '直接访问' },
+            { value: 310, name: '邮件营销' },
+            { value: 274, name: '联盟广告' },
+            { value: 235, name: '视频广告' },
+            { value: 400, name: '搜索引擎' }
+          ].sort((a, b) => a.value - b.value),
+          roseType: 'radius',
+          label: {
+            normal: {
+              textStyle: {
+                color: 'rgba(255, 255, 255, 0.3)'
+              }
+            }
+          },
+          labelLine: {
+            normal: {
+              lineStyle: {
+                color: 'rgba(255, 255, 255, 0.3)'
+              },
+              smooth: 0.2,
+              length: 10,
+              length2: 20
+            }
+          },
+          itemStyle: {
+            normal: {
+              color: '#c23531',
+              shadowBlur: 200,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          },
+
+          animationType: 'scale',
+          animationEasing: 'elasticOut',
+          animationDelay: _ => Math.random() * 200
+        }
+      ]
+    }
+  }
+  componentDidMount() {
+    this.setState({ load: true })
+  }
+  render() {
+    const { load } = this.state
+    return (
+      <>
+        <Card title='饼图一'>
+          { load ? <ReactEcharts option={this.getOption()} style={{ height: 300 }} /> : null }
+        </Card>
+        <Card title='饼图二'>
+          { load ? <ReactEcharts option={this.getOption2()} style={{ height: 300 }} /> : null }
+        </Card>
+      </>
+    )
   }
 }
